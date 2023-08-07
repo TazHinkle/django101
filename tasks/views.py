96e6535828fb8e46f153f9a6976a8f7ab99c8f61
Taz Hinkle <hinkle.taz@gmail.com>
2023-08-07 14:29:48 -0600

    use Django form generation and hook form to list

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
 
 # Create your views here.
 tasks = [
@@ -7,6 +10,9 @@ tasks = [
     "three",
 ]
 
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 def index(request):
     # python does not support JS shorthand for assigning key/value of the same name.
@@ -16,4 +22,16 @@ def index(request):
 
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
