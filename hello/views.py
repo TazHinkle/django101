302a5cda553752f63013b9a148a5f8205bfd6bce
Taz Hinkle <hinkle.taz@gmail.com>
2023-08-07 10:56:02 -0600

    Renders an html file

--- hello/views.py
+++ hello/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    return render(request, "hello/index.html")
 
 
 def taz(request):
